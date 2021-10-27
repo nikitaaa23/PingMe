bb430d0483aeb814b7b73904ddc99d02aee0c7e1
Nikita <saininikita23@gmail.com>
2021-10-27 13:16:50 +0530

    chat API

--- src/App.js
+++ src/App.js
@@ -2,3 +2,14 @@ import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
 
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="ad0e5ce1-467f-4197-9bc6-42d186475dc9"
+            userName="Nikita"
+            userSecret="baby"
+        />
+    )
+}
+export default App;
